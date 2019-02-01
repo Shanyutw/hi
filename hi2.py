03acf7a15af9278fdc9c8390c05dd95cc950909d
Derek Lu <dereklutw@gmail.com>
2019-02-01 15:32:46 +0800

    The new information

---
+++ hi2.py
@@ -0,0 +1,6 @@
+name = input('Please enter your name:')
+age = input('Please enter your age:')
+height = input('Please enter your height(cm):')
+number = input('Please enter your number:')
+
+print('Hi,Number',number,' ', name,' ',age,'years old',' ',height,'cm tall.' )
